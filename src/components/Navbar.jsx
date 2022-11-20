cc262a4dd51f58d648c523d3bda3755cc53f24bd
DeepakBarwal <deepakbarwal98@gmail.com>
2022-11-20 18:17:39 +0530

    added search bar and icons to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,23 @@
-import { Pets } from "@mui/icons-material";
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import { AppBar, styled, Toolbar, Typography, Box, InputBase, Badge, Avatar } from "@mui/material";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
   justifyContent: "space-between"
-})
+});
+
+const Search = styled("div")(({theme}) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%"
+}));
+
+const Icons = styled(Box)(({theme}) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "20px"
+}));
 
 const Navbar = () => {
   return (
@@ -12,6 +25,16 @@ const Navbar = () => {
       <StyledToolbar>
         <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>Navbar Brand</Typography>
         <Pets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search><InputBase placeholder="Search..." /></Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar sx={{ width: 30, height: 30 }} src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+        </Icons>
       </StyledToolbar>
     </AppBar>
   )
